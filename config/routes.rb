da74a7cc26c79855349fa32b3649ec2e11b4fc3d
mgi166 <skskoari@gmail.com>
2015-03-09 15:59:11 +0900

    * $ rails g scaffold_controller user_sessions --no-jbuilder --no-test-framework

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'app#home'
+  root to: 'app#index'
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
