127807a16ab185be787c6266a02dacf69ba6358e
mgi166 <skskoari@gmail.com>
2015-03-09 23:59:04 +0900

    * Add routes `resources :user_sessions, only: [:new, :create, :destroy]`

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'app#index'
 
   resources :users
+  resources :user_sessions, only: [:new, :create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
