fd4273673bd4435ca7f4ea5f1dfe0fc3e153f833
mgi166 <skskoari@gmail.com>
2015-03-18 22:30:02 +0900

    * Logout and redirect to root_url

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,6 +1,5 @@
 class UserSessionsController < ApplicationController
   include UserSessionsHelper
-  before_action :set_user_session, only: [:show, :edit, :update, :destroy]
 
   # GET /user_sessions/new
   def new
@@ -18,21 +17,12 @@ class UserSessionsController < ApplicationController
   end
 
   # DELETE /user_sessions/1
-  # DELETE /user_sessions/1.json
   def destroy
-    @user_session.destroy
-    respond_to do |format|
-      format.html { redirect_to user_sessions_url, notice: 'User session was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    logout
+    redirect_to root_url, notice: 'User session was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user_session
-      @user_session = UserSession.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_session_params
       params.require(:user_sessions).permit(:name, :password)
--- app/helpers/user_sessions_helper.rb
+++ app/helpers/user_sessions_helper.rb
@@ -3,6 +3,11 @@ module UserSessionsHelper
     @current_user ||= User.find(sessins[:user_id]) if sessins[:user_id]
   end
 
+  def logout
+    session.delete(:user_id)
+    @currnet_user = nil
+  end
+
   def login?
     !!current_user
   end
