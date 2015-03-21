48219a4a32b4df397b8ff5b58a19c43db41047fe
mgi166 <skskoari@gmail.com>
2015-03-21 22:45:40 +0900

    * $ rails g migration add_column_email_to_users email:string

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307052713) do
+ActiveRecord::Schema.define(version: 20150321134356) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
     t.string   "password",        limit: 255
-    t.string   "password_digest", limit: 255
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.string   "password_digest", limit: 255
   end
 
 end
