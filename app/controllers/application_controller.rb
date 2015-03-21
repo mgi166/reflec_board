b6499ee8ac122fd1ecd9d520d6d0f454830925ee
mgi166 <skskoari@gmail.com>
2015-03-21 23:25:58 +0900

    * move methods to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logout
+    session.delete(:user_id)
+    @currnet_user = nil
+  end
+
+  def login?
+    !!current_user
+  end
 end
--- app/helpers/user_sessions_helper.rb
+++ app/helpers/user_sessions_helper.rb
@@ -1,14 +1,2 @@
 module UserSessionsHelper
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logout
-    session.delete(:user_id)
-    @currnet_user = nil
-  end
-
-  def login?
-    !!current_user
-  end
 end
