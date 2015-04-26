e93a2d9f25f5b42e6955bd119846880833ac3983
mgi166 <skskoari@gmail.com>
2015-04-26 18:34:42 +0900

    require_login wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :require_user
+  before_action :require_login
 
   def current_user
     return @current_user_session if defined?(@current_user_session)
     @current_user_session = UserSession.find
   end
 
-  def require_user
+  def require_login
     unless current_user
       store_location
       redirect_to login_url, notice: "You must be logged out to access this page"
