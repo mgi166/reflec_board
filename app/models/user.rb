62972953945155384022cd7f04418f5a40acf794
mgi166 <skskoari@gmail.com>
2015-04-26 17:56:33 +0900

    acts_as_authentic to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,3 @@
 class User < ActiveRecord::Base
-  has_secure_password
-
-  validate :validate_name_or_email
-
-  def validate_name_or_email
-    if name.blank? && email.blank?
-      errors.add(:name_or_email, 'Should not blank both name and email.')
-    end
-  end
+  acts_as_authentic
 end
