77bbca34cf4d14d50d608cf61308a31f8ccef75e
mgi166 <skskoari@gmail.com>
2015-04-26 18:15:33 +0900

    Add #require_user, #store_location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :require_user
+
   def current_user
     return @current_user_session if defined?(@current_user_session)
     @current_user_session = UserSession.find
   end
 
-  def logout
-    session.delete(:user_id)
-    @currnet_user = nil
+  def require_user
+    unless current_user
+      store_location
+      redirect_to login_url, notice: "You must be logged out to access this page"
+    end
   end
 
-  def login?
-    !!current_user
+  def store_location
+    session[:return_to] = request.url
   end
 end
