558ec2dde2ca2819c2af9e9404e11e6f5a185146
mgi166 <skskoari@gmail.com>
2015-05-04 02:30:10 +0900

    Add feature spec

---
+++ spec/features/logins_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.feature "Logins", type: :feature do
+  describe '/login' do
+    before do
+      create(:user)
+    end
+
+    describe 'given currect username' do
+      it do
+        visit login_path
+
+        fill_in :username, with: 'test'
+        fill_in :password, with: 'password'
+        click_button 'login'
+
+        expect(page).to have_selector('div.alert-success')
+      end
+
+      it 'redirects to root_path' do
+        visit login_path
+
+        fill_in :username, with: 'test'
+        fill_in :password, with: 'password'
+        click_button 'login'
+
+        expect(current_path).to eq root_path
+      end
+    end
+
+    describe 'given bad username' do
+      it do
+        visit login_path
+
+        fill_in :username, with: 'bad-user'
+        fill_in :password, with: 'password'
+        click_button 'login'
+
+        expect(page).to have_selector('div.alert-danger')
+      end
+
+      it 'redirects to login_url' do
+        visit login_path
+
+        fill_in :username, with: 'bad-user'
+        fill_in :password, with: 'password'
+        click_button 'login'
+
+        expect(current_path).to eq login_path
+      end
+    end
+  end
+end
