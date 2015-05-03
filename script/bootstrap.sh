2cc268684448b423b2dc506b4356f221a32e8a02
mgi166 <skskoari@gmail.com>
2015-05-04 03:05:12 +0900

    Fix bootstrap.sh

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -4,5 +4,5 @@ if ! which mysql > /dev/null; then
   echo 'ERROR mysql does not be installed. Please install mysql.'
 fi
 
-bundle install --path vendor/bundle --binstubs vendor/bundle/bin
-rake db:drop db:create db:setup
+bundle install --path vendor/bundle --binstubs vendor/bundle/bin --jobs 4
+rake db:setup
