5e0d1190c1277932393f4b62184c3d4d1539196d
mgi166 <skskoari@gmail.com>
2015-05-04 03:00:18 +0900

    Add factory :musics

--- spec/factories/musics.rb
+++ spec/factories/musics.rb
@@ -1,6 +1,12 @@
 FactoryGirl.define do
   factory :music do
-    
+    sequence(:name) { |n| "music_#{n}" }
+    type "hard"
+    score 1000
+    achivement_rate 95.0
+    clear_level 1
+    play_count 0
+    miss_count 2
   end
 
 end
