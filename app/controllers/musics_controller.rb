64ff80ec424d1e65a2dc4a26a04edef0df9f16da
mgi166 <skskoari@gmail.com>
2015-05-04 19:46:58 +0900

    Show musics that current_user have

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -4,7 +4,7 @@ class MusicsController < ApplicationController
   # GET /musics
   # GET /musics.json
   def index
-    @musics = Music.all
+    @musics = current_user.musics
   end
 
   # GET /musics/1
