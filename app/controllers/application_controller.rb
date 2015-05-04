62556d7b04a0d45b5780fc2bf9a5ff121c5bf387
mgi166 <skskoari@gmail.com>
2015-05-04 10:35:08 +0900

    Fix login logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,18 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user, :logged_in?
 
-  def current_user
+  def current_user_session
     return @current_user_session if defined?(@current_user_session)
     @current_user_session = UserSession.find
   end
 
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.user
+  end
+
   def logged_in?
-    defined? @current_user_session
+    !! current_user
   end
 
   def require_login
