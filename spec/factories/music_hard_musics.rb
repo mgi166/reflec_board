95a061d18c1e8f7849e89b670261cbf767dff6af
mgi166 <skskoari@gmail.com>
2015-05-06 15:41:52 +0900

    Updates factory * Add attributes difficulty

--- spec/factories/music_hard_musics.rb
+++ spec/factories/music_hard_musics.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :hard_music, :class => 'Music::HardMusic' do
     sequence(:name) { |n| "hard_music_#{n}" }
     type "Music::HardMusic"
+    difficulty '10+'
     score { rand(3000) }
     achivement_rate { [70.0, 75.0, 80.0, 85.0, 90.0, 95.0, 100.0].sample }
     clear_level { (1..4).to_a.sample }
--- spec/factories/musics.rb
+++ spec/factories/musics.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :music do
     sequence(:name) { |n| "music_#{n}" }
     type "Music::HardMusic"
+    difficulty '10+'
     score 1000
     achivement_rate 95.0
     clear_level 1
