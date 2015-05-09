3e0b01b623f36c1382956fef24aa2dcc7116cec6
mgi166 <skskoari@gmail.com>
2015-05-09 09:44:51 +0900

    $ rails g scaffol_controller friends

---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,25 @@
+class FriendsController < ApplicationController
+  before_action :set_friend, only: [:show, :edit, :update, :destroy]
+
+  # GET /friends
+  # GET /friends.json
+  def index
+    @friends = Friend.all
+  end
+
+  # GET /friends/1
+  # GET /friends/1.json
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_friend
+      @friend = Friend.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def friend_params
+      params[:friend]
+    end
+end
