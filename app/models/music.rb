0364157d479e4f6d73a16ae2e755919875b627b9
mgi166 <skskoari@gmail.com>
2015-07-15 22:10:35 +0900

    Add associations between `music` and `score`

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,6 +1,7 @@
 class Music < ActiveRecord::Base
   has_many :music_users
   has_many :users, through: :music_users
+  has_many :scores
 
   scope :level_by, -> (*levels) { where(level: [levels].flatten) }
 end
