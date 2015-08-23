468a032c022bfdff4575a3c318ed4a70f470113c
mgi166 <skskoari@gmail.com>
2015-08-23 21:57:55 +0900

    Add MusicScoreCrawling#start

--- app/models/music_score_crawling.rb
+++ app/models/music_score_crawling.rb
@@ -1,3 +1,16 @@
 class MusicScoreCrawling < ActiveRecord::Base
   belongs_to :user
+
+  enum status: %w(pending start_failed start_successed)
+
+  def start
+    self.started_at = Time.current
+
+    @valanga = Valanga::Client.new
+
+    start_successed!
+  rescue
+    start_failed!
+    raise $!
+  end
 end
--- spec/models/music_score_crawling_spec.rb
+++ spec/models/music_score_crawling_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe MusicScoreCrawling, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#start' do
+    context 'when failed in start' do
+      let(:crawler) { build(:music_score_crawling, user: user) }
+      let(:user) { create(:user) }
+
+      it do
+        # TODO: Stub phantomjs http request
+        expect do
+          crawler.start
+        end.to raise_error Valanga::LoginError
+      end
+
+      it "status = `start_failed`" do
+        begin
+          crawler.start
+        rescue
+          expect(crawler.status).to eq "start_failed"
+        end
+      end
+    end
+  end
 end
